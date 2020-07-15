5e644b06cb7c8350ff445e94dd02ea6870dc5666
Jacob Eckert <jweckert17@gmail.com>
2020-07-15 15:50:07 -0500

    implemented another function in BusinessList.js to render a list of manufacturing businesses to the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import { BusinessList } from './BusinessList.js';
 import { BusinessListNewYork } from './BusinessList.js';
+import { BusinessListManufacturing } from './BusinessList.js';
 
 BusinessList();
 BusinessListNewYork();
+BusinessListManufacturing();
