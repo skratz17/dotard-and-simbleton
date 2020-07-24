ec55c215923a6bb9276e416053e532edff533373
Jacob Eckert <jweckert17@gmail.com>
2020-07-24 10:53:58 -0500

    major refactor of business list component to get DRYer

--- scripts/businesses/BusinessList.js
+++ scripts/businesses/BusinessList.js
@@ -2,11 +2,12 @@ import { useBusinesses, useBusinessesMatchingPropertyValue } from './BusinessPro
 import { Business } from './Business.js';
 
 const eventHub = document.querySelector('.container');
+const container = document.querySelector('.container');
 
 eventHub.addEventListener('searched', event => {
   const { searchTerm } = event.detail;
 
-  renderBusinessesMatchingName(searchTerm);
+  BusinessListWhere('businesses--found', 'Search Results', 'companyName', searchTerm);
 });
 
 /**
@@ -25,46 +26,43 @@ const renderListToDom = (domNode, businesses, listHeading) => {
 };
 
 /**
- * Render only those companies whose names match companyName parameter to article.businesses--found DOM node.
- * @param {String} companyName The company name to find matching companies for.
+ * Render a business list for all those businesses whose given property matches the given value.
+ * If a DOM node with class of className is not found on DOM, it will be appended to .container. Otherwise, the innerHTML of the existing DOM node with given className will be updated with new list.
+ * @param {String} className Class name that the list should use.
+ * @param {String} heading The heading that should display above the list
+ * @param {String} property The name of the property to render a list filtered by
+ * @param {String} value The value that the property should search on
  */
-const renderBusinessesMatchingName = companyName => {
-  const domNode = document.querySelector('.businesses--found');
+export const BusinessListWhere = (className, heading, property, value) => {
+  let domNode = document.querySelector(`.${className}`);
+  if(!domNode) {
+    domNode = createListArticleNode(className);
+    container.appendChild(domNode);
+  }
+  
+  const businesses = getBusinessList(property, value);
 
-  const matchingBusinesses = useBusinessesMatchingPropertyValue('companyName', companyName);
-
-  renderListToDom(domNode, matchingBusinesses, 'Matching Businesses');
+  renderListToDom(domNode, businesses, heading);
 };
 
 /**
- * Render all businesses to article.businesses--all DOM node.
+ * Create and return an <article> DOM node with classes 'list' and className
+ * @param {String} className The custom class name to give the element
  */
-export const BusinessList = () => {
-  const domNode = document.querySelector('.businesses--all');
-
-  const businesses = useBusinesses();
-
-  renderListToDom(domNode, businesses, 'All Businesses');
+const createListArticleNode = className => {
+  const domNode = document.createElement('ARTICLE');
+  domNode.classList.add('list');
+  domNode.classList.add(className);
+  return domNode;
 };
 
 /**
- * Render only businesses in New York to article.businesses--newYork DOM node.
+ * Given a property and value, return the proper list of businesses. If either property or value is undefined or empty, returns all businesses. Else, returns only those businesses whose value at the given property matches the value argument.
+ * @param {String} property
+ * @param {String} value 
  */
-export const BusinessListNewYork = () => {
-  const domNode = document.querySelector('.businesses--newYork');
+const getBusinessList = (property, value) => {
+  if(!property || !value) return useBusinesses();
 
-  const newYorkBusinesses = useBusinessesMatchingPropertyValue('addressStateCode', 'NY');
-
-  renderListToDom(domNode, newYorkBusinesses, 'New York Businesses');
-};
-
-/**
- * Render only businesses in Manufacturing to article.businesses--manufacturing DOM node.
- */
-export const BusinessListManufacturing = () => {
-  const domNode = document.querySelector('.businesses--manufacturing');
-
-  const manufacturingBusinesses = useBusinessesMatchingPropertyValue('companyIndustry', 'Manufacturing');
-
-  renderListToDom(domNode, manufacturingBusinesses, 'Manufacturing Businesses');
-};
+  return useBusinessesMatchingPropertyValue(property, value);
+}
